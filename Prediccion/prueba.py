0f55ceb441caec91c1dc0805c31273cecc62e0b2
Maxi Burastero <maxicc4@gmail.com>
2017-04-24 18:24:53 -0300

    Se llega a la conclusion de que hay un cambio horario en San Francisco dos veces al anio

--- Prediccion/prueba.py
+++ Prediccion/prueba.py
@@ -12,10 +12,20 @@ df['delta_duration'] = df.duration - (df.end_date - df.start_date).dt.total_seco
 print ("-----------")
 print (df.loc[:,['id','delta_duration']])
 #print (len(df.duration.unique()))
+# Total de viajes
 print (len(df))
+# Duracion de viajes dentro del rango esperado
 print (len(df[(df.delta_duration <= 60) & (df.delta_duration >= -60)]))
+# Duracion de viajes que caen fuera del rango esperado
 print (len(df[df.delta_duration > 60]))
 print (len(df[df.delta_duration < -60]))
+
+''' El delta_duration para los viajes que caen fuera del rango es de 
+3600 segundos (1 hora) aprox.
+Segun pude ver en internet en esas fechas justo hubo un cambio de horario,
+se adelanto o se atrazo una hora.
+https://www.timeanddate.com/time/change/usa/san-francisco?year=2014
+'''
 
 print ("-----------")
 
