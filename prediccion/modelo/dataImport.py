9c1cd6e26fb2d439e816b8f2a6d804392329379e
mfprado <pradomflorencia@gmail.com>
2017-05-16 21:29:26 -0300

    modulo para importar los csv

--- prediccion/modelo/dataImport.py
+++ prediccion/modelo/dataImport.py
@@ -1,16 +1,20 @@
 import pandas as pd
-import numpy as np
 
+def dataImport():
+	trip_train = pd.read_csv("../Data/trip_train.csv")
+	trip_train.start_date = pd.to_datetime(trip_train.start_date, format='%m/%d/%Y %H:%M') 
+	trip_train.end_date = pd.to_datetime(trip_train.end_date, format='%m/%d/%Y %H:%M')
 
-def dataImport(){
-	trip_train = pd.read_csv("Data/trip_train.csv")
-	trip_train.start_date = pd.to_datetime(df.start_date, format='%m/%d/%Y %H:%M') 
-	trip_train.end_date = pd.to_datetime(df.end_date, format='%m/%d/%Y %H:%M')
+	trip_test = pd.read_csv("../Data/trip_test.csv")
 
-	# trip_test = pd.read_csv("Data/trip_test.csv")
-		
-}
-
+	# weather = pd.read_csv("Data/weather.csv")  	
+	# station = pd.read_csv("Data/station.csv")
+	
+	# tanto weather como station son archivos que nos 
+	# permiten usar para la prediccion, si es necesario
+	# los usaremos
+	return (trip_train,trip_test)
+	
 #df['dura_min'] = (df.end_date - df.start_date).dt.seconds - 60
 #df['dura_max'] = df.dura_min + 120
 # trip_train['delta_duration'] = trip_train.duration - (trip_train.end_date - trip_train.start_date).dt.total_seconds()
