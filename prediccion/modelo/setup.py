f8639756b21a6d7ed4b4b12424151e13250dc55b
mfprado <pradomflorencia@gmail.com>
2017-05-16 21:47:47 -0300

    acomodo setup file

--- prediccion/modelo/setup.py
+++ prediccion/modelo/setup.py
@@ -3,15 +3,15 @@ try:
 except ImportError:
 	from distutils.core import setup
 
-	config = {
-		'description' = 'Machine Learning Competition'
-		'author' = 'Maximiliano, Galvan Nahuel, Pablo, Prado Maria Florencia'
-		'url': 'https://github.com/Gnahue/sanFranciscoBiclas.git',
-    	'download_url': 'https://github.com/Gnahue/sanFranciscoBiclas.git',
-    	'version': '0.1',
-    	'install_requires': ['nose'],
-    	'packages': ['modelo'],
-    	'scripts': [],
-    	'name': 'timePrediction'
-}
+config = {
+	'description' = 'Machine Learning Competition'
+	'author' = 'Maximiliano, Galvan Nahuel, Pablo, Prado Maria Florencia'
+	'url': 'https://github.com/Gnahue/sanFranciscoBiclas.git',
+	'download_url': 'https://github.com/Gnahue/sanFranciscoBiclas.git',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['modelo'],
+	'scripts': [],
+	'name': 'timePrediction'
+}	
 	
