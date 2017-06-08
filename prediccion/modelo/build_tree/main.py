c2ac1e5064134c9a04a8c5b8bd9e6b94ad20e39e
mfprado <pradomflorencia@gmail.com>
2017-06-08 14:31:50 -0300

    funcion para obtener predicciones de varios arboles

--- prediccion/modelo/build_tree/main.py
+++ prediccion/modelo/build_tree/main.py
@@ -1,21 +1,46 @@
 from classes import Tree
 import pandas as pd
+from data_import import data_import
 
 
-def get_predictions(df_test, tree):
-    results = []
+def get_tree_prediction(df_test, tree):
+    prediction = []
 
     for i in range(0, len(df_test)):
         df_register = df_test.ix[i:i]
-        # TODO: Ir guardandolo directamente en un csv
-        # por ahora se guarda en results como: (id , prediccion)
-        results.append((df_register.id.values[0], tree.get_prediction(df_register)))
 
-    return results
+        # por ahora se guarda en results como: (id , prediccion)
+        prediction.append((df_register.id.values[0], tree.get_prediction(df_register)))
+
+    return prediction
+
+
+def get_trees_prediction(df_test, trees):
+    predictions = []
+    # lista de las predicciones de todos los arboles
+    # ex:  [[(id1,32),(id2,45)], [(id1,23),(id2,35)]]
+
+    for tree in trees:
+        predictions.append(get_tree_prediction(df_test, tree))
+
+    # TODO:
+    # calcular el promedio de las predicciones y retornar el csv
+
+def build_trees(n, train, target, n_random_columns, max_depth):
+    trees = []
+    for i in range(0, n):
+        trees.append(Tree(train, target, n_random_columns, max_depth))
+
+    return trees
+
+
+def build_bagging_trees(n, train, target, max_depth):
+    # toma todas las columnas para hacer el split
+    return build_trees(n, train, target, (len(train.columns) - 1), max_depth)
 
 
 def main():
-    # (train,test) = data_import.data_import()
+    # (train, test) = data_import()
 
     # tree = Tree(train, 'duration', 4, 2)
 
@@ -24,14 +49,14 @@ def main():
     train = pd.read_csv('../../Data/youtube_train.csv')
     test = pd.read_csv('../../Data/youtube_test.csv')
 
-    tree = Tree(train, 'hours', 2, 6)  # esto es para bagging
+    tree = Tree(train, 'hours', 2, 3)
 
-    results = get_predictions(test, tree)
+    results = get_tree_prediction(test, tree)
+
     # tree.print_leafs()
-
     # prediction = tree.get_prediction(train.ix[5:5])
-    print results
 
+    print results
 
 
 main()
