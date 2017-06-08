70b166b9f6f4c81cc3d44f02d942bb143570f338
mfprado <pradomflorencia@gmail.com>
2017-06-08 03:04:26 -0300

    change dataImpot to data_import, para consistencia

--- prediccion/modelo/build_tree/data_import.py
+++ prediccion/modelo/build_tree/data_import.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os.path
 
-def dataImport():
+def data_import():
 
 
     # train["Age"] = train["Age"].fillna(train["Age"].median())  para valores faltantes
