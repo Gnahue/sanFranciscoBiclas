95a1aa8fd7bdf68fa0963b3ead5cbb24781403fd
Maxi Burastero <maxicc4@gmail.com>
2017-06-09 18:51:08 -0300

    Se separa el data_import en dos (uno para train y otro para test)

--- prediccion/modelo/algoritmo/data_import.py
+++ prediccion/modelo/algoritmo/data_import.py
@@ -2,35 +2,39 @@ import pandas as pd
 import numpy as np
 import os.path
 
-def data_import():
-    file_train = "../../Data/train.csv"
+def get_test():
     file_test = "../../Data/test.csv"
-    exist_file_train = os.path.isfile(file_train)
     exist_file_test = os.path.isfile(file_test)
 
-
-    if (not exist_file_train) or (not exist_file_test):
+    if not exist_file_test:
         weather = pd.read_csv("../../Data/weather.csv")
         weather = transform_data_weather(weather)
         station = pd.read_csv("../../Data/station.csv")
         station = transform_data_station(station)
-
-
-    if not exist_file_train:
-        trip_train = pd.read_csv("../../Data/trip_train.csv")
-        trip_train = transform_data_trip(trip_train, station, weather)
-        trip_train.to_csv(file_train, index=False)
-    else:
-        trip_train = pd.read_csv(file_train)
-    
-    if not exist_file_test:
         trip_test = pd.read_csv("../../Data/trip_test.csv")
         trip_test = transform_data_trip(trip_test, station, weather)
         trip_test.to_csv(file_test, index=False)
     else:
         trip_test = pd.read_csv(file_test)
 
-    return (trip_train,trip_test)
+    return trip_test
+
+def get_train():
+    file_train = "../../Data/train.csv"
+    exist_file_train = os.path.isfile(file_train)
+
+    if not exist_file_train:
+        weather = pd.read_csv("../../Data/weather.csv")
+        weather = transform_data_weather(weather)
+        station = pd.read_csv("../../Data/station.csv")
+        station = transform_data_station(station)
+        trip_train = pd.read_csv("../../Data/trip_train.csv")
+        trip_train = transform_data_trip(trip_train, station, weather)
+        trip_train.to_csv(file_train, index=False)
+    else:
+        trip_train = pd.read_csv(file_train)
+
+    return trip_train
 
 
 def transform_data_trip(trip, station, weather):
