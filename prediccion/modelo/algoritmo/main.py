31363a75af6803ab34992164f98310d775944f03
mfprado <pradomflorencia@gmail.com>
2017-06-11 23:54:06 -0300

    fix

--- prediccion/modelo/algoritmo/main.py
+++ prediccion/modelo/algoritmo/main.py
@@ -5,11 +5,16 @@ from data_import import get_test
 
 
 def main():
+    test = get_test()
 
-    trees = [] #los 100 arboles creados
-
-    for i in range(0, 50):
+    trees = []  # los 100 arboles creados
+    for i in range(0, 100):
         trees.append(desserialize_tree(str(i)+'RFFlorencia.pkl'))
+
+
+    test = get_test()
+    write_csv(get_trees_prediction(test, trees), 'results.csv')
+
 
     # for i in range(0, 25):
     #     trees.append(desserialize_tree(str(i)+'RFNahuel.pkl'))
@@ -19,14 +24,5 @@ def main():
     #
     # for i in range(0, 25):
     #     trees.append(desserialize_tree(str(i)+'RFPablo.pkl'))
-
-    test = get_test()
-
-    # test = test.head(10)
-
-    #definir la particion del test
-
-    write_csv(get_trees_prediction(test, trees))
-
 
 main()
