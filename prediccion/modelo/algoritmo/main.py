fada2124879552e33bf3f2879a2a297b45cc591e
mfprado <pradomflorencia@gmail.com>
2017-06-12 23:55:37 -0300

    crea csv por prediccion

--- prediccion/modelo/algoritmo/main.py
+++ prediccion/modelo/algoritmo/main.py
@@ -1,28 +1,22 @@
+
 from serialization import desserialize_tree
+from predictions import get_tree_prediction
 from predictions import get_trees_prediction
 from predictions import write_csv
 from data_import import get_test
 
 
 def main():
-    test = get_test()
-
-    trees = []  # los 100 arboles creados
-    for i in range(0, 100):
-        trees.append(desserialize_tree(str(i)+'RFFlorencia.pkl'))
-
 
     test = get_test()
-    write_csv(get_trees_prediction(test, trees), 'results.csv')
+
+    for i in range(0, 500):
+
+    	tree = desserialize_tree(str(i)+'RF_5_5.pkl')  #ahi pongan el nombre de los arboles
+    	write_csv(get_tree_prediction(test, tree).items(), str(i) + 'prediction.csv')  #aca va el nombre de como se crean los csv de resultados
 
 
     # for i in range(0, 25):
     #     trees.append(desserialize_tree(str(i)+'RFNahuel.pkl'))
-    #
-    # for i in range(0, 25):
-    #     trees.append(desserialize_tree(str(i)+'RFMaximiliano.pkl'))
-    #
-    # for i in range(0, 25):
-    #     trees.append(desserialize_tree(str(i)+'RFPablo.pkl'))
 
 main()
